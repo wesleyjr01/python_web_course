100709f85ac4313f1109b06d046b04479ba3c78d
Wesley Bortolozo <wesleyjr0101@gmail.com>
2020-05-02 21:06:00 -0300

    added lectures 92,93 and 94

---
+++ Section5-2019PriceAlertsOnlineWebStore/94-PageShowAllItems/models/alert.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+from models.item import Item
+from models.model import Model
+
+
+class Alert(Model):
+
+    collection = "alerts"
+
+    def __init__(self, item_id: str, price_limit: float, _id: str = None):
+        super().__init__(_id)
+        self.item_id = item_id
+        self.item = Item.find_by_id(self.item_id)
+        self.price_limit = price_limit
+
+    def json(self) -> Dict:
+        return {
+            "_id": self._id,
+            "price_limit": self.price_limit,
+            "item_id": self.item_id,
+        }
+
+    def load_item_price(self) -> float:
+        return self.item.load_price()
+
+    def notify_if_price_reached(self) -> None:
+        if self.item.price <= self.price_limit:
+            print(
+                f"Item {self.item} has reached a price under {self.price_limit}.\nLatest price: {self.item.price}"
+            )
---
+++ Section5-2019PriceAlertsOnlineWebStore/94-PageShowAllItems/views/items.py
@@ -0,0 +1,29 @@
+""" This file is going to contain our item's blueprint
+that will contain all the endpoints related to items."""
+
+import json
+from flask import Blueprint, render_template, request, make_response
+from models.item import Item
+
+item_blueprint = Blueprint("items", __name__)
+
+
+@item_blueprint.route("/")
+def index():
+    items = Item.find_all()
+    return render_template("items/index.html", items=items)
+
+
+@item_blueprint.route("/new", methods=["GET", "POST"])
+def new_item():
+    """ we are accessing fields names inside requests.form"""
+    if request.method == "POST":
+        url = request.form["url"]
+        tag_name = request.form["tag_name"]
+        query = json.loads(request.form["query"])
+
+        Item(url, tag_name, query).save_to_mongo()
+
+        return make_response(index())
+
+    return render_template("items/new_item.html")
